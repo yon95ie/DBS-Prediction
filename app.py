105623b084033fe49403928a29d331eec70b0543
Yong Jie Low <68228282+yon95ie@users.noreply.github.com>
2022-04-23 16:56:11 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask, render_template, request
+
+
+# In[2]:
+
+
+app = Flask(__name__)
+
+
+# In[3]:
+
+
+import joblib
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    if request.method == "POST":
+        rate = float(request.form.get("rate"))
+        model = joblib.load("DBS_Prediction")
+        pred = float(model.predict([[rate]]))
+        return(render_template("index.html", result=pred))
+    else:
+        return(render_template("index.html", result="awaiting input"))
+
+
+# In[ ]:
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
